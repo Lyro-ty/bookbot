fae99ef1be4346f7f17d74edc267660dc02d7e33
Lyro-ty <curtisbldrn@gmail.com>
2025-05-03 23:25:26 -0600

    added filepath argument

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+import sys
 from stats import word_count
 from stats import character_count
 from stats import alphabetize_dict
 
+
+
 def get_book_text(filepath):
-    with open(filepath) as f:
+    with (open(filepath)) as f:
         return f.read()
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
+    book_text = get_book_text(filepath)
     num_words = word_count(book_text)
     char_count = character_count(book_text)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt")
+    print(f"Analyzing book found at {filepath}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
